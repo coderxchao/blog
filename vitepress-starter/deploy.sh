71ac3df1be7f67e1c694aaa7921a968f4b6d89a5
coderxchao <coderxchao@outlook.com>
2023-11-04 14:47:16 +0800

    publish

--- vitepress-starter/deploy.sh
+++ vitepress-starter/deploy.sh
@@ -18,7 +18,8 @@ git commit -m 'deploy'
 # git push -f https://github.com/coderxchao/blog.git main:gh-pages
 
 # 因为有些git版本默认创建的分支为master分支，有的为main分支，所以这里直接创建并切换到gh-pages分支
-git checkout -b gh-pages
+git branch -m gh-pages
+git checkout gh-pages
 # git push -f https://github.com/coderxchao/blog.git
 git push -f --set-upstream https://github.com/coderxchao/blog.git
 
